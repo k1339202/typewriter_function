1cd3c8083a63caf952d1fc6bc866a396999f5c3b
George <k1339202@students.katyisd.org>
2025-03-31 09:33:42 -0500

    Update typewriter.py

--- typewriter.py
+++ typewriter.py
@@ -3,6 +3,8 @@ import time
 
 def typewriter(string)
   for char in string:
+    # Change these to increase the effect variance
+    #                    v   v
     time.sleep(uniform(0.1,0.25))
     sys.stdout.write(char)
     sys.stdout.flush()
